069136c95667d2b82c724d02c86acc10a657ec57
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-02-09 03:26:43 +0800

    Update design

--- Organize/DIP/ItemAddFormView+Design.swift
+++ Organize/DIP/ItemAddFormView+Design.swift
@@ -152,30 +152,6 @@ private struct SkeletonView: View {
         }
         .ignoresSafeArea()
     }
-    
-    var colorGridTestView : some View {
-        Section {
-            LazyHGrid(rows: [GridItem(.fixed(30.00), spacing: 15), GridItem(.fixed(30.00), spacing: 15)], spacing: 20, content: {
-                Circle().foregroundStyle(.red)
-                Circle().foregroundStyle(.orange)
-                Circle().foregroundStyle(.yellow)
-                Circle().foregroundStyle(.green)
-                Circle().foregroundStyle(.mint)
-                Circle().foregroundStyle(.cyan)
-                Circle().foregroundStyle(.blue)
-                Circle().foregroundStyle(.red)
-                Circle().foregroundStyle(.orange)
-                Circle().foregroundStyle(.yellow)
-                Circle().foregroundStyle(.green)
-                Circle().foregroundStyle(.mint)
-                Circle().foregroundStyle(.cyan)
-                Circle().foregroundStyle(.blue)
-                ColorPicker("Color", selection: .constant(.accent))
-                    .labelsHidden()
-            })
-            .frame(maxWidth: .infinity)
-        }
-    }
 }
 
 private struct HiFiView : View {
@@ -212,6 +188,144 @@ private struct HiFiView : View {
                     .listRowSpacing(0)
 //                    .listRowBackground(Color.blue)
                     .listRowInsets(.init())
+                    DisclosureGroup(
+                                            isExpanded: .constant(true),
+                                            content: {
+                                                Section {
+                                                    Grid(horizontalSpacing: 30, verticalSpacing: 20) {
+                                                        GridRow {
+                                                            Circle().foregroundStyle(.red)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.orange)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.yellow)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.green)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.cyan)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.blue)
+                                                                .frame(height: 30)
+                                                            
+                                                        }
+                                                        
+                                                        GridRow {
+                                                            Circle().foregroundStyle(.purple)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.pink)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.mint)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.brown)
+                                                                .frame(height: 30)
+                                                            Circle().foregroundStyle(.gray)
+                                                                .frame(height: 30)
+                                                            ColorPicker("Color", selection: .constant(.accent))
+                                                                .labelsHidden()
+                                                        }
+                                                    }
+                                                    .frame(maxWidth: .infinity)
+                                                    .padding(.vertical, 5)
+                                                }
+                                                
+                                                Section {
+                                                    Grid(horizontalSpacing: 20, verticalSpacing: 20) {
+                                                        GridRow {
+                                                            ForEach(0...5, id: \.self) { _ in
+                                                                ZStack {
+                                                                    Circle().foregroundStyle(Color(uiColor: UIColor.secondarySystemBackground))
+                                                                    Image(systemName: Item.randomSystemSymbol)
+                                                                }
+                                                                .frame(height: 40)
+                                                            }
+                                                            
+                                                        }
+                                                        GridRow {
+                                                            GridRow {
+                                                                ForEach(0...5, id: \.self) { _ in
+                                                                    ZStack {
+                                                                        Circle().foregroundStyle(Color(uiColor: UIColor.secondarySystemBackground))
+                                                                        Image(systemName: Item.randomSystemSymbol)
+                                                                    }
+                                                                    .frame(height: 40)
+                                                                }
+                                                                
+                                                            }
+                                                        }
+                                                    }
+                                                    .frame(maxWidth: .infinity)
+                                                    .padding(.vertical, 5)
+                                                }
+                                                
+                                                Section {
+                                                    ViewThatFits {
+                                                        HStack(spacing: 20) {
+                                                            PatternCardView(design: PatternDesign.getRandomDesign(), color: color)
+                                                                .frame(height: 120)
+                                                                .padding(.vertical)
+                                                                .frame(minWidth: 120)
+                                                            PatternCardView(design: PatternDesign.getRandomDesign(), color: color)
+                                                                .frame(height: 120)
+                                                                .padding(.vertical)
+                                                                .frame(minWidth: 120)
+                                                            PatternCardView(design: PatternDesign.getRandomDesign(), color: color)
+                                                                .frame(height: 120)
+                                                                .padding(.vertical)
+                                                                .frame(minWidth: 120)
+                                                            Button {
+                                                                
+                                                            } label: {
+                                                                Image(systemName: "arrow.clockwise.circle.fill")
+                                                                    .symbolRenderingMode(.hierarchical)
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .frame(width: 50, height: 50)
+                                                                    .padding()
+                                                            }
+
+                                                        }
+                                                        
+                                                        VStack() {
+                                                            PatternCardView(design: PatternDesign.getRandomDesign(), color: color)
+                                                                .frame(height: 120)
+                                                                .padding(.vertical)
+                                                            PatternCardView(design: PatternDesign.getRandomDesign(), color: color)
+                                                                .frame(height: 120)
+                                                                .padding(.vertical)
+                                                            PatternCardView(design: PatternDesign.getRandomDesign(), color: color)
+                                                                .frame(height: 120)
+                                                                .padding(.vertical)
+                                                            
+                                                            Button {
+                                                                
+                                                            } label: {
+                                                                Image(systemName: "arrow.clockwise.circle.fill")
+                                                                    .symbolRenderingMode(.hierarchical)
+                                                                    .resizable()
+                                                                    .scaledToFit()
+                                                                    .frame(width: 30, height: 30)
+                                                            }
+                                                        }.padding(.vertical)
+                                                    }
+                                                } header: {
+                                                    Text("Pattern")
+                                                        // cancel out indentation caused by disclosure group
+                                                        .offset(x: -20)
+                                                }
+                                            },
+                                            label: {
+                                                Label {
+                                                    Text("Styles")
+                                                } icon: {
+                                                    Image(systemName: "paintbrush")
+                                                        .resizable()
+                                                        .scaledToFit()
+                                                        .foregroundStyle(.white)
+                                                        .frame(width: 28, height: 28)
+                                                }
+                                                .labelStyle(ShapedLabelStyle(shape: .roundedRectangle(6), backgroundColor: .orange))
+                                            }
+                                        )
                     Section {
                         // Picker styles in Menu is pretty cool but this kinda
                         // need to be in
@@ -232,91 +346,7 @@ private struct HiFiView : View {
                             .labelStyle(ShapedLabelStyle(shape: .roundedRectangle(6), scaleEffect: 0.6, backgroundColor: .pink))
                         }
                     }
-                    DisclosureGroup(
-                        content: {
-                            Section {
-                                Grid(horizontalSpacing: 30, verticalSpacing: 20) {
-                                    GridRow {
-                                        Circle().foregroundStyle(.red)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.orange)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.yellow)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.green)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.cyan)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.blue)
-                                            .frame(height: 30)
-
-                                    }
-                                    
-                                    GridRow {
-                                        Circle().foregroundStyle(.purple)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.pink)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.mint)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.brown)
-                                            .frame(height: 30)
-                                        Circle().foregroundStyle(.gray)
-                                            .frame(height: 30)
-                                        ColorPicker("Color", selection: .constant(.accent))
-                                            .labelsHidden()
-                                    }
-                                }
-                                .frame(maxWidth: .infinity)
-                                .padding(.vertical, 5)
-                            }
-                            
-                            Section {
-                                Grid(horizontalSpacing: 20, verticalSpacing: 20) {
-                                    GridRow {
-                                        ForEach(0...5, id: \.self) { _ in
-                                            ZStack {
-                                                Circle()                                            .foregroundStyle(Color(uiColor: UIColor.secondarySystemBackground))
-                                                Image(systemName: Item.randomSystemSymbol)
-                                            }
-                                                .frame(height: 40)
-                                        }
-
-                                    }
-                                    GridRow {
-                                        GridRow {
-                                            ForEach(0...5, id: \.self) { _ in
-                                                ZStack {
-                                                    Circle()                                            .foregroundStyle(Color(uiColor: UIColor.secondarySystemBackground))
-                                                    Image(systemName: Item.randomSystemSymbol)
-                                                }
-                                                    .frame(height: 40)
-                                            }
-
-                                        }
-                                    }
-                                }
-                                .frame(maxWidth: .infinity)
-                                .padding(.vertical, 5)
-                            }
-                        },
-                        label: {
-                            Label {
-                                Text("Styles")
-                            } icon: {
-                                Image(systemName: "paintbrush")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .foregroundStyle(.white)
-                                    .frame(width: 28, height: 28)
-                            }
-                            .labelStyle(ShapedLabelStyle(shape: .roundedRectangle(6), backgroundColor: .orange))
-                        }
-                    )
-                    
                 }
-                .listSectionSpacing(15)
-                
                 
             }
             .navigationTitle(title)
---
+++ Organize/Views/SubViews/PatternCardView.swift
@@ -0,0 +1,36 @@
+//
+//  PatternCardView.swift
+//  Organize
+//
+//  Created by Yuhao Chen on 2/9/24.
+//
+
+import SwiftUI
+
+struct PatternCardView: View {
+    let design: PatternDesign
+    let color: Color
+    var body: some View {
+        ZStack {
+            PatternDesignView(design, patternColor: color)
+                .clipShape(RoundedRectangle(cornerRadius: 25))
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(
+                    LinearGradient(
+                        stops: [
+                            .init(color: .white, location: 0.1),
+                            .init(color: color.opacity(0.2), location: 0.5),
+                            .init(color: Color.clear, location: 1)
+                        ],
+                        startPoint: .topLeading,
+                        endPoint: .bottom), lineWidth: 1.5)
+        }
+        .shadow(color: color.opacity(0.7), radius: 12)
+    }
+}
+
+#Preview {
+    PatternCardView(design: PatternDesign.getRandomDesign(), color: .accent)
+        .frame(height: 200)
+        .padding()
+}
