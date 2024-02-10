d604d46b0e6e503ef51481ab57584c8d49926e67
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-02-10 16:43:22 +0800

    Implemented title and IconNameCardView for FormEditView

--- Organize/Views/Forms/FormEditView.swift
+++ Organize/Views/Forms/FormEditView.swift
@@ -5,21 +5,52 @@
 //  Created by Yuhao Chen on 2/10/24.
 //
 
+import OSLog
 import SwiftUI
 
 struct FormEditView: View {
     @Binding var target: any Meta
+    var mode: FormMode = .add
+    var title: String {
+        guard let targetString = {
+            switch target {
+            case is Item:       "Item"
+            case is Storage:    "Storage"
+            case is Space:      "Space"
+            default:            nil
+            }
+        }() else {
+            logger.warning("Title is empty for FormEditView because Target is not a implemented type.")
+            return ""
+        }
+        
+        let modeString = mode.rawValue
+        return (modeString + " an " + targetString)
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
                 List {
-                    
+                   IconNameCardView(target)
                 }
             }
+            .navigationTitle(title)
+            .navigationBarTitleDisplayMode(.inline)
         }
         .ignoresSafeArea()
     }
 }
+
+extension FormEditView {
+    enum FormMode: String {
+        case edit   = "Edit"
+        case add    = "Add"
+        case create = "Create"
+    }
+}
+
+fileprivate let logger = Logger(subsystem: OrganizeApp.bundleId, category: "FormEditView")
 
 #Preview {
     FormEditView(
