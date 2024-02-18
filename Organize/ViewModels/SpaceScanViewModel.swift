28a9e3504df84d43e28b08aa35559e9ca664eeec
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-02-19 03:53:43 +0800

    Added ViewModel for ScanSpace

--- Organize/ViewModels/SpaceScanViewModel.swift
+++ Organize/ViewModels/SpaceScanViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by Yuhao Chen on 2/19/24.
 //
 
+import Combine
 import Foundation
+import RoomPlan
+
+// TODO: Upgrade to using observation framework
+class SpaceScanViewModel: ObservableObject {
+    enum Action {
+        case startSession
+        case share
+        case export
+    }
+    
+    // TODO: Is Combine really necessary here? I dislike an observer pattern sticking inside of MVVM pattern
+    // TODO: These are really data models, URLs should be using the same filemanager system CapturedObject is using
+    var actions = PassthroughSubject<Action, Never>();
+    var exportURL = FileManager.default.temporaryDirectory.appending(path: "ScannedSpace.usdz")
+    let metadataURL = FileManager.default.temporaryDirectory.appending(path: "ScannedSpace.json")
+    
+    @Published var canExport = false
+    @Published var showShareSheet = false
+    @Published var capturedRoom: CapturedRoom?
+}
